7508f73d98ca7e39e09b6aa50c65b34059d340d2
linktlh <linktlh@gmail.com>
2022-02-01 17:08:45 -0600

    Version change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="bnptool",
-    version="1.0.1a",
+    version="1.0.2",
     author="linktlh",
     author_email="linktlh@gmail.com",
     description="A simple Commandline interface to create BNPs using BCML",
